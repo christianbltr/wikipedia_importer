640f965d0aa04ed867aee49f3d869c7f81fb90ad
Christian Bülter <buelter@conquer.de>
2020-05-08 12:12:55 +0200

    [TASK] remove obsolete files; fix typo

--- ext_tables.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if (TYPO3_MODE === 'BE') {
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers']['WikipediaImporter-Importer'] =
-        \Pluswerk\WikipediaImporter\Command\WikipediaImporterCommandController::class;
-}
